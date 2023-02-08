5536a36ed05e489a7ac31d617479599afb6500f3
coolcuber <35880825+coolcuber@users.noreply.github.com>
2023-02-08 18:44:06 -0500

    Update checkErrors.py

--- checkErrors.py
+++ checkErrors.py
@@ -1,8 +1,9 @@
 def checkErrors(testsAndMessages):
     for test in testsAndMessages.keys():
         if (not test):
-            raise Excpetion(testsAndMessages(test))
+            raise Exception(testsAndMessages(test))
 
+# Seemed like a better idea at the time
 def isType(obj, *types):
     for type in types:
         if (isinstance(obj, type)):
